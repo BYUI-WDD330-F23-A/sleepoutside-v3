b41721c4adce94c0da518f8ad349e13c99698f2c
Mike Lewis <7482464+Wyoming-C64@users.noreply.github.com>
2023-09-23 21:04:56 -0600

    Repaired setLocalStorage function.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -11,7 +11,16 @@ export function getLocalStorage(key) {
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+  let cartContents = localStorage.getItem(key);
+  if (!cartContents) {
+    cartContents = new Array();
+  } else {
+    cartContents = JSON.parse(cartContents);
+  }
+  cartContents.push(data);
+  localStorage.setItem(key, JSON.stringify(cartContents));
+  console.log(key);
+  console.log(JSON.stringify(data));
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
