0488a9033900ed009da0c2c082dff38df1fdc8e3
Mike Lewis <7482464+Wyoming-C64@users.noreply.github.com>
2023-09-30 23:16:01 -0600

    Lint and format code.

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,31 +4,34 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || []; // Need to assign an empty array if LocalStorage is null (empty).
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  
+
   // cartItems will now never be NULL. More appropriate to check for empty array.
   if (cartItems.length > 0) {
-    //console.log("The cart has something"); 
-    let totalItems = cartItems.map(function(item){
+    //console.log("The cart has something");
+    let totalItems = cartItems.map(function (item) {
       let total = [];
-      total.push(item.FinalPrice); 
-      return total; 
-    }); 
-    document.querySelector(".checkout-area").innerHTML = checkoutTemplate(totalItems);
+      total.push(item.FinalPrice);
+      return total;
+    });
+    document.querySelector(".checkout-area").innerHTML =
+      checkoutTemplate(totalItems);
   }
 }
 
-function checkoutTemplate(items){
-  let sumTotal = 0; 
-  items.forEach(value => {sumTotal += parseFloat(value)}); 
+function checkoutTemplate(items) {
+  let sumTotal = 0;
+  items.forEach((value) => {
+    sumTotal += parseFloat(value);
+  });
   /* console.log("Checking")
   console.log(sumTotal); */
-  
+
   const totalPrice = `<div class="cart-footer-hide">
   <p class="cart-total">Total: $${sumTotal}</p>
   <button id="checkoutBtn">Checkout</button>
-</div>`
-return totalPrice; 
-}; 
+</div>`;
+  return totalPrice;
+}
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
