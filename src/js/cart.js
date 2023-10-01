c36bd46f592b3861ca5cbaad24ed0a44da596496
Mike Lewis <7482464+Wyoming-C64@users.noreply.github.com>
2023-09-30 22:59:58 -0600

    Fix initialization of cartItems to be not NULL.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,7 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || []; // Need to return an empty array if LocalStorage is empty.
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
   
