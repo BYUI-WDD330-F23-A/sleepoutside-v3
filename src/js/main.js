ee6c3f49d3df16d7a4c24f9cd3fcd06aeade0e0c
hectapia <oli21007@byui.edu>
2023-10-12 22:16:12 -0600

    Add header and footer as js templates

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 /// We're going to add some code here in main.
 import { default as productList } from "./productList.mjs";
 import { pageAlert } from "./alert.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 /// We might initialized some variables.
 /// We might call an API...
@@ -8,3 +9,5 @@ productList("prodList", "tents");
 
 /// We might output some values.
 pageAlert();
+
+loadHeaderFooter();
