a0c8590ed4f008966e05d470a069a9e87871c3ab
hectapia <oli21007@byui.edu>
2023-10-12 23:30:15 -0600

    Clean all code

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,46 +1,6 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 import  shoppingList   from "./shoppingCart.mjs";
 
-function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart") || []; // Need to assign an empty array if LocalStorage is null (empty).
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-
-  // cartItems will now never be NULL. More appropriate to check for empty array.
-  if (cartItems.length > 0) {
-    //console.log("The cart has something");
-    let totalItems = cartItems.map(function (item) {
-      let total = [];
-      total.push(item.FinalPrice);
-      return total;
-    });
-    document.querySelector(".checkout-area").innerHTML =
-      checkoutTemplate(totalItems);
-  }
-}
-
-
-
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
-}
-
 loadHeaderFooter();
-//renderCartContents();
 shoppingList("shopping-list");
 
--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -1,50 +1,5 @@
 import { renderListWithTemplate, getLocalStorage } from "./utils.mjs";
 
-
-function renderCartContents() {
-    const cartItems = getLocalStorage("so-cart") || []; // Need to assign an empty array if LocalStorage is null (empty).
-    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  
-
-}  
-
-function checkoutTemplate(items) {
-    let sumTotal = 0;
-    items.forEach((value) => {
-      sumTotal += parseFloat(value);
-    });
-    /* console.log("Checking")
-    console.log(sumTotal); */
-  
-    const totalPrice = `<div class="cart-footer-hide">
-    <p class="cart-total">Total: $${sumTotal}</p>
-    <button id="checkoutBtn">Checkout</button>
-  </div>`;
-    return totalPrice;
-  }
-
-export default function shoppingList(selector = "") {
-  //TODO add something 
-  const itemListArea = document.getElementById(selector); 
-  const products =  getLocalStorage("so-cart");  
-
-        // cartItems will now never be NULL. More appropriate to check for empty array.
-        if (products.length > 0) {
-            //console.log("The cart has something");
-            let totalItems = products.map(function (item) {
-              let total = [];
-              total.push(item.FinalPrice);
-              return total;
-            });
-            document.querySelector(".checkout-area").innerHTML =
-              checkoutTemplate(totalItems);
-          }
-
-  renderListWithTemplate(shoppingCartTemplate, itemListArea, products); 
-
-}
-
 function shoppingCartTemplate(item) {
     const newItem = `<li class="cart-card divider">
     <a href="#" class="cart-card__image">
@@ -64,4 +19,36 @@ function shoppingCartTemplate(item) {
     return newItem;
   }
 
+function checkoutTemplate(items) {
+    let sumTotal = 0;
+    items.forEach((value) => {
+      sumTotal += parseFloat(value);
+    });
 
+  
+    const totalPrice = `<div class="cart-footer-hide">
+    <p class="cart-total">Total: $${sumTotal}</p>
+    <button id="checkoutBtn">Checkout</button>
+  </div>`;
+    return totalPrice;
+  }
+
+export default function shoppingList(selector = "") {
+  //TODO add something 
+  const itemListArea = document.getElementById(selector); 
+  const products =  getLocalStorage("so-cart");  
+
+        // cartItems will now never be NULL. More appropriate to check for empty array.
+        if (products.length > 0) {
+            let totalItems = products.map(function (item) {
+              let total = [];
+              total.push(item.FinalPrice);
+              return total;
+            });
+            document.querySelector(".checkout-area").innerHTML =
+              checkoutTemplate(totalItems);
+          }
+
+  renderListWithTemplate(shoppingCartTemplate, itemListArea, products); 
+
+}
