3b85ce2ab0f3e2de595183894bbe81441a74566e
Edgar Cobian <edgarcobian0304@gmail.com>
2023-10-24 12:53:17 -0700

    added the functionally for not adding more cart

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,80 +1,101 @@
 import { findProductById } from "./productData.mjs";
-import { setLocalStorage } from "./utils.mjs";
-import { iconAnimation } from "./shoppingCart.mjs"; 
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { iconAnimation } from "./shoppingCart.mjs";
 
 let theProduct = {};
 
 export default async function productDetails(productId) {
+  theProduct = (await findProductById(productId)) || false;
 
-    theProduct = await findProductById(productId) || false;
-    
-    if (theProduct) {
-        renderProductDetails(theProduct);
-        // Add the event listener now that the page is rendered.
-        document.getElementById("addToCart").addEventListener('click', addProductToCart);
-        document.getElementById("addToCart").addEventListener("click", iconAnimation);
-    } else {
-        renderNotFound();
-    }
+  if (theProduct) {
+    renderProductDetails(theProduct);
+    // Add the event listener now that the page is rendered.
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", addProductToCart);
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", iconAnimation);
+  } else {
+    renderNotFound();
+  }
 }
 
 export function addProductToCart() {
+  // this won't add multiple items when hitting the add to cart button.
+  let cart_items;
+  let matched_index;
+  cart_items = getLocalStorage("so-cart");
+  if (cart_items !== null) {
+    cart_items.forEach((item, i) => {
+      if (item.Id == theProduct.Id) {
+        matched_index = i;
+      }
+    });
+  }
+  if (matched_index !== undefined) {
+  } else {
     setLocalStorage("so-cart", theProduct);
   }
+}
 
 function renderNotFound() {
-    const productName = document.getElementById('productName');
-    const productNameWithoutBrand = document.getElementById('productNameWithoutBrand');
-    const productImage = document.getElementById('productImage');
-    const productFinalPrice = document.getElementById('productFinalPrice');
-    const productColorName = document.getElementById('productColorName');
-    const productDescriptionHtmlSimple = document.getElementById('productDescriptionHtmlSimple');
-    const buttonContainer = document.getElementById('buttonContainer');
+  const productName = document.getElementById("productName");
+  const productNameWithoutBrand = document.getElementById(
+    "productNameWithoutBrand"
+  );
+  const productImage = document.getElementById("productImage");
+  const productFinalPrice = document.getElementById("productFinalPrice");
+  const productColorName = document.getElementById("productColorName");
+  const productDescriptionHtmlSimple = document.getElementById(
+    "productDescriptionHtmlSimple"
+  );
+  const buttonContainer = document.getElementById("buttonContainer");
 
-    productName.innerHTML = "We've searched all over, but...";
-    productNameWithoutBrand.innerHTML = "... We can't find that. &#128546;";
-    productImage.src = "../images/crossroads.jpg";
-    productImage.alt = "A crossroads sign.";
-    productFinalPrice.innerHTML = "";
-    
-    productColorName.innerHTML = "";
-    
-    productDescriptionHtmlSimple.innerHTML = 
-        `We are very sorry, but it appears the product you have requested is
+  productName.innerHTML = "We've searched all over, but...";
+  productNameWithoutBrand.innerHTML = "... We can't find that. &#128546;";
+  productImage.src = "../images/crossroads.jpg";
+  productImage.alt = "A crossroads sign.";
+  productFinalPrice.innerHTML = "";
+
+  productColorName.innerHTML = "";
+
+  productDescriptionHtmlSimple.innerHTML = `We are very sorry, but it appears the product you have requested is
         not available. If the manufacturer has not discontinued the product,
         please check back at a later date to see if we have restocked this item.`;
 
-    buttonContainer.innerHTML = "";
+  buttonContainer.innerHTML = "";
 
-    return false;
+  return false;
 }
 
 function renderProductDetails(product) {
+  const productName = document.getElementById("productName");
+  const productNameWithoutBrand = document.getElementById(
+    "productNameWithoutBrand"
+  );
+  const productImage = document.getElementById("productImage");
+  const productFinalPrice = document.getElementById("productFinalPrice");
+  const productColorName = document.getElementById("productColorName");
+  const productDescriptionHtmlSimple = document.getElementById(
+    "productDescriptionHtmlSimple"
+  );
+  const addToCartButton = document.getElementById("addToCart");
 
-    const productName = document.getElementById("productName");
-    const productNameWithoutBrand = document.getElementById("productNameWithoutBrand");
-    const productImage = document.getElementById("productImage");
-    const productFinalPrice = document.getElementById("productFinalPrice");
-    const productColorName = document.getElementById("productColorName");
-    const productDescriptionHtmlSimple = document.getElementById("productDescriptionHtmlSimple");
-    const addToCartButton = document.getElementById("addToCart");
+  productName.innerHTML = product.Name;
+  productNameWithoutBrand.innerHTML = product.NameWithoutBrand;
+  productImage.src = product.Images.PrimaryLarge;
+  productImage.alt = product.NameWithoutBrand;
+  productFinalPrice.innerHTML = "$" + product.FinalPrice;
 
-    productName.innerHTML = product.Name;
-    productNameWithoutBrand.innerHTML = product.NameWithoutBrand;
-    productImage.src = product.Images.PrimaryLarge;
-    productImage.alt = product.NameWithoutBrand;
-    productFinalPrice.innerHTML = "$" + product.FinalPrice;
+  const colorList = product.Colors.map((aColor) => aColor.ColorName);
 
-    const colorList = product.Colors.map(
-        (aColor) => aColor.ColorName
-    );
-    
-    let colorListHtml = colorList.join("<br>");
-    productColorName.innerHTML = colorListHtml;
-    
-    productDescriptionHtmlSimple.innerHTML = product.DescriptionHtmlSimple;
+  let colorListHtml = colorList.join("<br>");
+  productColorName.innerHTML = colorListHtml;
 
-    addToCartButton.dataset.id = product.Id;
+  productDescriptionHtmlSimple.innerHTML = product.DescriptionHtmlSimple;
 
-    return product;
+  addToCartButton.dataset.id = product.Id;
+
+  return product;
 }
