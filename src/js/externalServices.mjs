62d169d915ef0099dec56d7d85a52ffa26ae29a5
Edgar Cobian <edgarcobian0304@gmail.com>
2023-10-26 19:21:11 -0700

    Refactor code

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -2,7 +2,7 @@ import { convertToJson } from "./utils.mjs";
 
 const baseURL = import.meta.env.VITE_SERVER_URL
 
-export async function getData(category) {
+export async function getProductsByCategory(category) {
   const response = await fetch(baseURL + `products/search/${category}`);
   const data = await convertToJson(response);
   return data.Result;
